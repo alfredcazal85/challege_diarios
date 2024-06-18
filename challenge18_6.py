1883bb012c23db2454c594fa8577a246129844bd
alfredcazal85 <169089369+alfredcazal85@users.noreply.github.com>
2024-06-18 17:55:06 +0200

    Add files via upload

---
+++ challenge18_6.py
@@ -0,0 +1,3 @@
+celsius = float (input("Que temperatura hace en Grados Celsius:"))
+fahrenheit= ((celsius*9/5)+32)
+print (f"{celsius} grados celsius son equivalentes a {fahrenheit} grados fahrenheit")
